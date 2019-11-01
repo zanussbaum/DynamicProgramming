76da7f837face7f2f2401ee79ccd9c9caa584ad7
Zach <zanussbaum@davidson.edu>
2019-10-31 23:10:37 -0400

    greedy and dynamic coin

--- coins.py
+++ coins.py
@@ -16,24 +16,79 @@ import sys
 def read_input():
     data = sys.stdin.read().strip()
     coins = [int(c) for c in data.split()]
+    return coins
 
 def upper_bound(coins):
     """Determines the maximum value that needs to be checked to
     prove coins canonical."""
-    raise NotImplementedError("TODO")
+    return coins[-1] * 2
 
 def greedy(coins, value):
     """Determines the number of coins the greedy algorithm uses
-    to make change for value."""
-    raise NotImplementedError("TODO")
+    to make change for value.
+    
+    params:
+        coins: list of coin denominations
+        value: change to make
+
+    returns:
+        int: number of coins
+
+        Can we assume that we are also able to make correct change?
+    """
+    change = value
+
+    num_coins = 0
+    for coin in coins[::-1]:
+        if change <= 0:
+            break
+        if change >= coin:
+            num_coins += change//coin
+            change = change % coin
+    
+    return num_coins
+
+def get_min(coins, change_list, current_change):
+    min_value = float('inf')
+
+    for coin in coins:
+        if current_change - coin >= 0:
+            if change_list[current_change - coin] < min_value:
+                min_value = change_list[current_change - coin]
+
+    return min_value
 
 def dynamic(coins, value):
-    """Determines the optimal number of coins to make change for value."""
-    raise NotImplementedError("TODO")
+    """Determines the optimal number of coins to make change for value.
+    
+     
+    params:
+        coins: list of coin denominations
+        value: change to make
+
+    returns:
+        int: number of coins
+    """
+    change = value
+
+    change_list = [0] * (change + 1)
+
+    for i in range(1, change + 1):
+        change_list[i] = 1 + get_min(coins, change_list, i)
+
+    return change_list[-1]
+
 
 def main():
     coins = read_input()
-    #TODO: call your greedy and dynamic functions on 1...upper_bound
+    upper = upper_bound(coins)
+
+    for i in range(1, upper + 1):
+        if dynamic(coins, i) != greedy(coins, i):
+            print("NON-CANONICAL")
+            return
+
+    print('CANONICAL')
 
 if __name__ == "__main__":
     main()
