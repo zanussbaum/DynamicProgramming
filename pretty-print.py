86d460dde005733ff880e113b14bddbdffc813d2
aidanoneill3776 <aidanoneill3776@gmail.com>
2019-11-03 00:08:02 -0400

    3b

--- pretty-print.py
+++ pretty-print.py
@@ -15,6 +15,42 @@ Usage: ./pretty-print.py M inputFile
 
 from sys import argv,exit
 
+def calculatePenalty(words, M):
+  tot_len = 0
+  for word in words:
+    tot_len += len(word) + 1
+  tot_len -= 1
+  if tot_len > M:
+    return None
+  return (M - tot_len) ** 2
+
+def pretty_print(words, M):
+  sols = [0] *( len(words)+1)
+  solution_dict = {}
+  for i in range(len(words)-1, -1, -1):
+    best = None
+    for j in range(i, len(words)):
+      temp = calculatePenalty(words[i:j+1], M)
+      if temp is None:
+        continue
+
+      temp = temp + sols[j+1]
+      if best is None or temp < best:
+        best = temp
+        sols[i] = best
+        solution_dict.update({i: j+1})
+
+  walker = 0
+  while walker< len(words):
+    sub_str = ""
+    for i in range(walker, solution_dict[walker]):
+      sub_str += words[i] + ' '
+    walker = solution_dict[walker]
+
+    print(sub_str)
+  print(sols[0])
+  return sols[0]
+
 def readFile(fname):
   """
   input: the name of a text file which contains words separated by
@@ -25,7 +61,7 @@ def readFile(fname):
   wordList = []
   for l in f:
       if (len(l) <= 1): # the line was empty
-          continue;
+          continue
       else:
           wordList += l.strip().split(' ')
   return wordList
@@ -33,13 +69,13 @@ def readFile(fname):
 
 def main():
   if len(argv) != 3:
-    print "usage %s <number> <inputFile>" % argv[0]
+    print("usage %s <number> <inputFile>" % argv[0])
     exit()
 
   M = int(argv[1])
   words = readFile(argv[2])
+  pretty_print(words, M)
 
-  #TODO: pretty-print the text using dynamic programming
   
 
 if __name__ == "__main__":
